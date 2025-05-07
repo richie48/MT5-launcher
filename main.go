f8e631619ea1688f90420d0a74545391d18fa7d6
richie48 <odiorrichard@gmail.com>
2025-05-07 14:50:43 +0100

    opened config file

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import ( "fmt"
+		 "os")
 
 func main() {
     fmt.Println("Hello, World!")
+
+	jsonFile, err := os.Open("config.json")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer jsonFile.Close()
+
+	fmt.Println("opened json file", jsonFile)
 }
