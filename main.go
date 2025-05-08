00d6423629e166e55970a544d668621b893d33ed
richie48 <odiorrichard@gmail.com>
2025-05-08 01:34:13 +0100

    now passing json to struct object

--- main.go
+++ main.go
@@ -1,20 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 )
 
-func main() {
-	fmt.Println("Hello, World!")
+type Account struct {
+	Name     string
+	Login    string
+	Password string
+	Server   string
+	Path     string
+}
 
-	jsonFile, err := os.Open("config.json")
+func main() {
+	jsonFile, err := os.Open("config-example.json")
 
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	fmt.Println("opened json file", jsonFile)
+
 	defer jsonFile.Close()
 
-	fmt.Println("opened json file", jsonFile)
+	byteValue, err := io.ReadAll(jsonFile)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var accounts []Account
+	if err := json.Unmarshal(byteValue, &accounts); err != nil {
+		fmt.Println(err)
+	}
+
+	for _, account := range accounts {
+		fmt.Printf("account name: " + account.Name)
+	}
 }
