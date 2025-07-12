bcbcc2e35e4867f9e7f22d38183c1afca495564c
richie48 <odiorrichard@gmail.com>
2025-07-12 21:50:37 +0100

    use test function, set host as 0.0.0.0

--- test_client.py
+++ test_client.py
@@ -3,22 +3,25 @@ from mt5linux_updated import MetaTrader5
 import json
 import os
 
+def test_metatrader5_works_on_linux():
+    mt5_client = MetaTrader5(host="0.0.0.0", port=17001)
 
-mt5_client = MetaTrader5()
+    with open("config.json", "r") as file:
+        config_data_list = json.load(file)
 
-with open("config.json", "r") as file:
-    config_data_list = json.load(file)
+    BASE_DIRECTORY = os.getenv("BASE_DIR")
 
-BASE_DIRECTORY = os.getenv("BASE_DIR")
+    for config_data in config_data_list:
+        instance_executable_path = os.path.join(BASE_DIRECTORY, config_data["Path"])
+        authorized = mt5_client.initialize(
+            path=instance_executable_path,
+            login=config_data["Login"],
+            server=config_data["Server"],
+            password=config_data["Password"],
+        )
 
-for config_data in config_data_list:
-    instance_executable_path = os.path.join(BASE_DIRECTORY, config_data["Path"])
-    authorized = mt5_client.initialize(
-        path=instance_executable_path,
-        login=config_data["Login"],
-        server=config_data["Server"],
-        password=config_data["Password"],
-    )
+        print("{0} got connected!".format(config_data["Name"]))
+        assert mt5_client.account_info() is not None
 
-    print("{0} got connected!".format(config_data["Name"]))
-    assert mt5_client.account_info() is not None
+if __name__ == "__main__": 
+    test_metatrader5_works_on_linux()
